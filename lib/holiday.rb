725099fe121818cc250ae02d663ce0c56376303e
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-13 14:10:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,7 +84,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season_hash, value|
+    puts "#{season_hash.string}"
+    binding.pry
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
