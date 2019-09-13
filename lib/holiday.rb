6fefe4e12ffdc5b44f2e5dd01419cc7e594a9f8a
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-13 13:57:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do |season_hash, value|
 
     if season_hash == season
-      season_hash.update({holiday_name => supply_array})
+      holiday_hash[season].update({holiday_name => supply_array})
       #binding.pry
     end
   end
